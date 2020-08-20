e8223ae7d62d8742ccb25b06644b62429adf9c92
Seyoung Yoon <stacey.exp@gmail.com>
2020-08-19 22:34:28 -0700

    Updates

---
+++ precache-manifest.7700e034970ebb69cab0e1f8e3ecad0d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06f10dc47149ef81b0f34e704605f765",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "a393d08094606117814c",
+    "url": "/react-portfolio/static/css/2.54d67f59.chunk.css"
+  },
+  {
+    "revision": "bcbce08554146c174c71",
+    "url": "/react-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a393d08094606117814c",
+    "url": "/react-portfolio/static/js/2.c5b6070a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.c5b6070a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcbce08554146c174c71",
+    "url": "/react-portfolio/static/js/main.f98a9789.chunk.js"
+  },
+  {
+    "revision": "7ab49a546a33138f93d9",
+    "url": "/react-portfolio/static/js/runtime-main.bc6ed5e9.js"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/react-portfolio/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "2fe6d179ea5aa1ceab26113e77e13da4",
+    "url": "/react-portfolio/static/media/react-bootstrap-carousel.2fe6d179.svg"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/react-portfolio/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/react-portfolio/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  }
+]);
