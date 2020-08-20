e54f5bb5c7bc3bd00d25a65f2debd42368c5d380
Seyoung Yoon <stacey.exp@gmail.com>
2020-08-20 00:04:26 -0700

    main page

--- src/pages/main.js
+++ src/pages/main.js
@@ -2,23 +2,34 @@ import React from 'react';
 import Project from "../components/card"
 import Navb from "../components/nav/navbar"
 import Footer from "../components/footer"
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
+
+
 
 // import './App.css';
 import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
 
 
 class Main extends React.Component {
-    render(){
+    render() {
         return (
             <div>
-                <Navb/>
-        
-                <Footer/>
+                <Navb />
+                <Jumbotron fluid>
+                    <Container>
+                        <h1>Hey Stacey here.</h1>
+                        <p>
+                            Images are currently broken, but all the links are working. Feel free to browse.
+    </p>
+                    </Container>
+                </Jumbotron>
+                <Footer />
             </div>
-            
+
         );
     }
-    
+
 }
 
 export default Main;
